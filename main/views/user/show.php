4f7141ab495480bbf19a5532dc9c04b661383240
João Gabriel Oliveira <joaogabrieloliveira81@gmail.com>
2020-11-26 21:16:58 -0300

    Improve render to get views files

---
+++ main/views/user/show.php
@@ -0,0 +1,2 @@
+<h1>Olá mundo</h1>
+<?php echo '<p>User:'.  $data['id'] . '</p>';
--- src/ControllerBase.php
+++ src/ControllerBase.php
@@ -2,24 +2,20 @@
 namespace Hyper;
 class ControllerBase
 {
-    protected $view;
+    protected $controller;
 
     public function render($view, $data = null)
     {
         $this->getNameView();
-        $view_path = dirname(__DIR__) . "\\main\\views\\$this->view\\$view.php";
-        if (file_exists($view_path))
-        {
-            echo "Arquivo existe: \"$view_path\"\n";
-            include $view_path;
-        }
-        else  echo "Não existe \"$view_path\"";
+        $view_path = dirname(__DIR__) . "\\main\\views\\$this->controller\\$view.*";
+        $view_path = glob($view_path)[0];
+        include_once $view_path;
     }
 
     protected function getNameView()
     {
         $class_name = strtolower(basename(str_replace("\\", DIRECTORY_SEPARATOR, get_called_class())));
         $pattern = '/(controller)/';
-        $this->view = preg_replace($pattern,'',$class_name);
+        $this->controller = preg_replace($pattern,'',$class_name);
     }
 }
